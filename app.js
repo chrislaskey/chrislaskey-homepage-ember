2af0919eb9d316d92b1cba1f9d681f7ea82a75de
Chris Laskey <contact@chrislaskey.com>
2013-05-01 18:22:28 -0400

    Page skeleton in Ember routes

--- app.js
+++ app.js
@@ -5,17 +5,31 @@ App.Router.reopen({
 });
 
 App.Router.map(function() {
+	this.route('technicalskills', {path: "/technical-skills"});
 	this.route('work', {path: "/work"});
+	this.route('blog', {path: "/blog"});
 });
 
 App.IndexRoute = Ember.Route.extend({
 	model: function() {
-		return ['red', 'yellow', 'blue'];
+		return ['this', 'is', 'home'];
+	}
+});
+
+App.TechnicalskillsRoute = Ember.Route.extend({
+	model: function(){
+		return ['this', 'is', 'technical skills'];
 	}
 });
 
 App.WorkRoute = Ember.Route.extend({
 	model: function(){
 		return ['this', 'is', 'work'];
+	}
+});
+
+App.BlogRoute = Ember.Route.extend({
+	model: function(params){
+		return ['this', 'is', 'blog'];
 	}
 });
